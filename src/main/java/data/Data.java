ba88d7163e14f382b1e5b1af25cad1c72b6b0641
senaytugce <senaytugcebaskin@gmail.com>
2025-08-04 04:57:26 +0300

    add pom

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -2,6 +2,6 @@ package data;
 
 public class Data {
     public String url = "https://www.trendyol.com/giris";
-    public String email= "xxxx@gmail.com";
-    public String password ="sifre123";
+    public String email= "senay.s.t.b@gmail.com";
+    public String password ="123Deneme*";
 }
